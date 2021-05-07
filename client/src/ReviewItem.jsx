8cfbc0c3174dec49494828d03a1bee4497cd5af1
Christopher Raffaele <alumnye@gmail.com>
2021-05-06 21:55:08 -0400

    Completed conditional rendering of stars based on rating.

--- client/src/ReviewItem.jsx
+++ client/src/ReviewItem.jsx
@@ -2,6 +2,26 @@ import React from 'react';
 import styles from './styles/ReviewItem.module.css';
 
 const ReviewItem = (props) => {
+  // conditional render function for star rating
+  console.log(props.review.starRating);
+
+  const renderStars = () => {
+    const filledStar = <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span>
+
+    const hollowStar = <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="4"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M12,6.47l1.21,2.84.41,1,1.05.09,3.07.27-2.32,2-.8.69.24,1,.69,3L12.9,15.79l-.9-.53-.9.53L8.45,17.38l.69-3,.24-1-.8-.69-2.32-2,3.07-.27,1.05-.09.41-1L12,6.47m.46-3.39h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4-.28-.88-6-.52L12.46,3.08Z"></path></svg></span>
+
+    let rating = props.review.starRating;
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < rating) {
+        stars.push(filledStar);
+      } else {
+        stars.push(hollowStar);
+      }
+    }
+    return stars;
+  }
+
   return (
     <div className={styles.reviewItem}>
 
@@ -20,12 +40,13 @@ const ReviewItem = (props) => {
             <div className={styles.innerContainer} css="body-text" css="margin-right-48px">
               <div className={styles.starsContainer} css="margin-left-6px">
                 <span className={styles.starsSpan} css="stars-span">
-                  {/* {props.review.starRating} */}
+                  {renderStars()}
+
+                    {/* <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span>
                     <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span>
                     <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span>
                     <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span>
-                    <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span>
-                    <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span>
+                    <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span> */}
 
                   </span>
                 </div>
