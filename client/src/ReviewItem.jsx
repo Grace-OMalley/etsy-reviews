1404d1718d4af8ec2c7faf195d81f8e282da78ec
Christopher Raffaele <alumnye@gmail.com>
2021-05-06 22:01:48 -0400

    code cleanup

--- client/src/ReviewItem.jsx
+++ client/src/ReviewItem.jsx
@@ -41,13 +41,6 @@ const ReviewItem = (props) => {
               <div className={styles.starsContainer} css="margin-left-6px">
                 <span className={styles.starsSpan} css="stars-span">
                   {renderStars()}
-
-                    {/* <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span>
-                    <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span>
-                    <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span>
-                    <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span>
-                    <span class="etsy-icon wt-nudge-b-1 wt-icon--smaller" data-rating="0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="3 3 18 18" aria-hidden="true" focusable="false"><path d="M20.83,9.15l-6-.52L12.46,3.08h-.92L9.18,8.63l-6,.52L2.89,10l4.55,4L6.08,19.85l.75.55L12,17.3l5.17,3.1.75-.55L16.56,14l4.55-4Z"></path></svg></span> */}
-
                   </span>
                 </div>
               </div>
